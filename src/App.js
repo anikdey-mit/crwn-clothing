08ce68d784584527cec5960a66f48eb99913cf4b
Anik Dey Sarker <anikdeysarker@Aniks-MacBook-Pro.local>
2021-06-30 16:40:00 +0600

    design shop page and adding header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
+import Header from './header/header.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>    
   );
